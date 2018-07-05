4798afde466ed2dd04ead3860863de19de54bb75
Pedro Buarque Santos <buarqu.p@wustl.edu>
2018-07-04 23:31:20 -0300

    s1

--- PDI/urls.py
+++ PDI/urls.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # coding: utf8
 
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,9 +9,9 @@ from PDI.core_1.views import deteccao
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-                       url(r'^deteccao/', 'PDI.core_1.views.deteccao', name='deteccao'),
-                       url(r'^admin/', admin.site.urls),)
+urlpatterns = ['',
+               url(r'^deteccao/', 'PDI.core_1.views.deteccao', name='deteccao'),
+               url(r'^admin/', admin.site.urls)]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
